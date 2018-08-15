749a578a3579f418a22d95d02bf1b5d7d2869d57
Unknown <kt.johnson111@outlook.com>
2018-08-15 13:05:42 -0600

    ProjectPg
    Added another project

--- src/Skills.js
+++ src/Skills.js
@@ -17,13 +17,18 @@ class Skills extends Component {
             <p> I don't know how to pull in my past projects from Github but maybe I'll take SS's and just use them as links to the repositories </p>
 
       <div className="container">
-        <img src= "namebadgess.png" alt="WOW" height="650" width="1050" />
+        <img src= "namebadgess.png" alt="namebadge" height="500" width="550" />
         <div className="overlay">
           <div class="text">Name Badge assignment where I practiced using state and props within a text area.</div>
         </div>
       </div>
-
-            </div>
+          <div className="container">
+            <img src="fffss.png" alt="funfoodfriendsapp" height="550" width="950" />
+              <div className="overlay">
+                <div class = "text"> My practice storing and retrieving data from a firebase database.  </div>
+              </div>
+          </div>
+          </div>
         )
     }
 }
