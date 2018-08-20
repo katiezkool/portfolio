a5e3e647b408d6cc1a7301288d4dbdfe730489ec
Unknown <kt.johnson111@outlook.com>
2018-08-20 13:49:32 -0600

    Repo Links
    added img links to repo's

--- src/Skills.js
+++ src/Skills.js
@@ -14,37 +14,47 @@ class Skills extends Component {
 
 
             <h1>My Projects</h1>
-            <p> To Do: add links to corresponding repos in img's</p>
+            <p> I made all of these projects for some extra practice using the techniques I've been learning the past few months. </p>
 
           <div className="container">
+            <a href="https://github.com/katiezkool/ghibli-api">
             <img src="ghibliss.png" alt="studioghibliapi" height="550" width="950" />
             <div className="overlay">
               <div className="text"> I used a tutorial to help me connect to an API in order to receive and display data.</div>
         </div>
+            </a>
       </div>
           <div className="container">
+            <a href = "https://github.com/katiezkool/FFFList">
             <img src="fffss.png" alt="funfoodfriendsapp" height="550" width="950" />
               <div className="overlay">
                 <div class = "text"> My practice storing and retrieving data from a firebase database.  </div>
               </div>
+            </a>
           </div>
           <div className="container">
+            <a href = "https://github.com/katiezkool/NameBadge">
             <img src="namebadgess.png" alt="namebadge" height="500" width="550" />
             <div className="overlay">
               <div className="text">Name Badge assignment where I practiced using state and props within a text area.</div>
             </div>
+            </a>
           </div>
           <div className="container">
+            <a href = "https://github.com/katiezkool/Blackjacko">
             <img src="blackjackss.png" alt="blackjackgame" height="600" width="350" />
             <div className="overlay">
               <div className="text">I made a simple blackjack game to further my JavaScript knowledge.</div>
           </div>
+            </a>
           </div>
           <div className="container">
+            <a href= "https://github.com/katiezkool/ReactTicTacToe">
             <img src="tictactoss.png" alt="tictactogame" height="500" width="650" />
             <div className="overlay">
               <div className="text">I also made a tic-tac-toe game to gain a better understanding of React.</div>
             </div>
+            </a>
           </div>
         </div>
         )
